cd6d7c26d1a50045e7c13c7e17f0a856dde58202
ivchenkodima <ivchenkodima@gmail.com>
2015-05-13 01:59:38 +0300

    init lesson 3:append()...

--- jquery/app2.js
+++ jquery/app2.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	// traversing
 	$("#vacations").find(".america");
+    
     $(".vacation").first();
+	$('#vacations li').last();
 
+	$("#tours").children('li');
+	$("#tours").find('li');
+
+	
 });
