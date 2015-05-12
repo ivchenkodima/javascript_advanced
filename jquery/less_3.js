5f5a202ba4ff3aad723deaa8ebee6a88c85f4ca3
ivchenkodima <ivchenkodima@gmail.com>
2015-05-13 02:12:10 +0300

    last one:done

--- jquery/less_3.js
+++ jquery/less_3.js
@@ -5,5 +5,13 @@ $(document).ready(function(){
 	$(".usa").append(message);
 	$('.book').remove();
 
+// removing the button and add price 
+$('button').on('click',function(){
+	var message = $("<span>Call 1-555-jquery-air to book this tour</span>");
+	$(".usa").append(message);
+	$("button").remove();
+});
+
+
 
 });
