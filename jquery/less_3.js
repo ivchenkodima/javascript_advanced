5d4be3caa3e383354aa7c58ea1e2ed5ea3578ed0
ivchenkodima <ivchenkodima@gmail.com>
2015-05-13 02:26:01 +0300

    correction

--- jquery/less_3.js
+++ jquery/less_3.js
@@ -12,6 +12,29 @@ $('button').on('click',function(){
 	$("button").remove();
 });
 
+$(document).ready(function(){
+  $("button").on("click", function(){
+    var message = $("<span>Call 1-555-jquery-air to book this tour</span>");
+    $(".usa").append(message);
+    $(this).remove();
+  });
+});
+
+$(document).ready(function(){
+  $('button').on('click', function(){
+    var message = $("<span>Call 1-555-jquery-air to book this tour</span>");
+    $(this).closest('.tour').append(message);
+    $(this).remove();
+    
+  });
+});
+
+$(document).ready(function(){
+  $(".tour").on("click", function(){
+    var message = $("<span>Call 1-555-jquery-air to book this tour</span>");
+    $(this).append(message).find('button').remove();
+   });
+});
 
 
 });
