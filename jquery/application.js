4c2c406622770f19092349e85233a5008781f76d
ivchenkodima <ivchhenkodima@gmail.com>
2015-05-12 23:38:14 +0300

    step 1

---
+++ jquery/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+$('h2').text("main");
+$('.vacation');
+
+
+});
--- nodejs/main.js
+++ nodejs/main.js
@@ -27,9 +27,11 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200);
+  response.writeHead(200, {
+  	 'Content-Type': 'text/plain'
+  });
   fs.readFile('index.html',function(err, contents){
   	response.write(contents);
-  });
   response.end();
+  });
 }).listen(8080);
